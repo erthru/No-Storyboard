52fa316d2e1ceee0b5a80d49feab4396df4ddd14
Suprianto D <ersaka96@gmail.com>
2019-03-14 19:59:02 +0800

    Initial Commit

---
+++ No Storyboard/MainController.swift
@@ -0,0 +1,21 @@
+//
+//  MainController.swift
+//  No Storyboard
+//
+//  Created by Suprianto Djamalu on 14/03/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Main"
+        
+    }
+    
+}
